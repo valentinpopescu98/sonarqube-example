c4e64410ca74ad58c970e51282f7f121dc4d90d8
Valentin Popescu <MariusValentin.Popescu@endava.com>
2022-08-10 11:00:00 +0300

    Added employee with address

---
+++ src/main/java/Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private Address address;
+
+    public Employee(String firstName, String lastName, Integer age, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", address=" + address +
+                '}';
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        xds
+
+        Employee employee = new Employee("Valentin", "Popescu", 24,
+                new Address("Aleea Mioritei", 3, "Potcoava",
+                        "Olt", "Romania", 237355));
+
+        System.out.println(employee);
     }
 }
